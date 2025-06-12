05b9fae2b91d3894612deebfe6f1e22531985c10
mosfetnik <adityarai424@gmail.com>
2025-06-12 19:30:37 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-/* eslint-disable no-undef */
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  rules: {
-    "react/prop-types": 0,
   },
 });
