89c3fc07fc04fa1332bc46ffaad24541ec19e2ff
mosfetnik <adityarai424@gmail.com>
2025-06-18 23:13:04 +0530

    remove logger

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -4,11 +4,5 @@ export const supabaseUrl = import.meta.env.VITE_SUPERBASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPERBASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const { data, error } = await supabase
-  .storage
-  .from('<bucket-name>')
-  .getPublicUrl('<file-path>')
-
-console.log(data?.publicUrl, error)
 
 export default supabase;
